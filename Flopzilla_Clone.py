427f6af18de2d2d36e13a884fe0625358484e4f6
IJustWannaLearnSomething <mohitkik2@gmail.com>
2018-06-10 04:56:54 +0530

    Hoja yaar ab to

---
+++ Flopzilla_Clone.py
@@ -0,0 +1,35 @@
+import random
+
+Values= "A23456789TJQK"
+Suits= "shcd"
+
+class Card:
+    def __init__(self, Value, Suit):         
+        self.Value = Value                    #(integer)Value of card, Can Be 1-13
+        self.Suit = Suit                      #(string) Suit of card, Can Be H, D, C or S
+        
+class Player:
+    def __init__(self, Position, isDealer, holeCard):
+        self.Position = Position             #(integer)Position of the player wrt Dealer
+        self.isDealer = isDealer             #(boolean) True if player is dealer
+        self.holeCard = holeCard             #(Card) Player's holding
+        
+class Board:
+    def __init__(self, run):                 #A list of 5 cards from Deck
+        self.run = run
+                           
+Deck = []                                    #list of objects of class Card
+for i in Values:                             #Initializing cards to Deck
+    for j in Suits:
+        Deck.append(Card(i, j))
+            
+num_players = int(input("Enter nuber of players (2-10): "))
+
+if num_players in range(2,11):               #Dealing?
+    iters = 0
+    while iters < num_players:
+        index = random.randint(0, 52)
+        print(Deck[index].Value + Deck[index].Suit)
+        iters += 1
+else:
+    print("Number of players either less than 2 or greater than 10!!")
